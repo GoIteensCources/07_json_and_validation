58df753449715386fa61df34131a67e3551f7724
Walentin <valoliinykov.work@gmail.com>
2024-10-11 16:16:51 +0300

    validation model

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from typing import Any
+
 from fastapi import FastAPI, Query, status
 from fastapi.responses import RedirectResponse, JSONResponse
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field, field_validator, model_validator
 
 app = FastAPI()
 
@@ -17,13 +19,28 @@ class Item(BaseModel):
     tax: float = Field(5, description="Нaлог на item", ge=0, lt=100)
     inn: str            # number and alpha
 
-
-    @field_validator("inn")
+    @field_validator("inn", )
     @classmethod
     def valid_inn(cls, v: str):
         if len(v) != 10:
             raise ValueError("len of field 'inn' must be 10")
-        return v
+        elif v.isalnum() is False:
+            raise ValueError("field 'inn' only alnum")
+        return v.upper()
+
+    @model_validator(mode="after")
+    @classmethod
+    def set_description(cls, data: Any):
+        if data.description == "string":
+            data.description = "good description"
+        return data
+
+    @model_validator(mode="before")
+    @classmethod
+    def inn2_ommited(cls, data: Any):
+        print(data)
+        assert ("inn2" not in data), "inn2 not include in data"
+        return data
 
 
 
@@ -34,6 +51,5 @@ async def create_item(item: Item, flag_save: bool = Query(True, description="set
     # item_json: str = item.model_dump_json()
     item_dict["price_with_tax"] = item.price + item.price*item.tax/100
     if flag_save:
-        print("Create")
         return JSONResponse(status_code=status.HTTP_201_CREATED, content=item_dict)
     return JSONResponse(status_code=status.HTTP_200_OK, content=item_dict)
--- temp.py
+++ temp.py
@@ -3,15 +3,19 @@ import json
 data: dict = {
     "id": 1,
     "is_auth": True,
+    "some": "some 2",
     "tegs": ["ad", 'ab']
 }
 
-data_json = json.dumps(data, indent=4)
-print(type(data_json))
-print(data_json)
+print("some 2" in list(data.keys()))
 
-data_2 = json.loads(data_json)
-print(data_2)
 
-data = str(data)
-print(type(data))
+# data_json = json.dumps(data, indent=4)
+# print(type(data_json))
+# print(data_json)
+#
+# data_2 = json.loads(data_json)
+# print(data_2)
+#
+# data = str(data)
+# print(type(data))
